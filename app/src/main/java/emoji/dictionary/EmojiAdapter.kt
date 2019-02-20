48fe8b1b2d9496fd101fff6d786e9a5fa7bdd11d
Alden <aj.molina092@gmail.com>
2019-02-20 09:21:04 -0500

    Setup adapter

--- app/src/main/java/emoji/dictionary/EmojiAdapter.kt
+++ app/src/main/java/emoji/dictionary/EmojiAdapter.kt
@@ -1,23 +1,34 @@
 package emoji.dictionary
 
 import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
 class EmojiAdapter : RecyclerView.Adapter<EmojiAdapter.TextHolder> {
-    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): TextHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun onCreateViewHolder(parent: ViewGroup, p1: Int): TextHolder {
+        return TextHolder(LayoutInflater.from(parent.context).inflate(R.layout.recyclerview_item_row, parent, false))
     }
 
     override fun getItemCount(): Int {
         return 10
     }
 
-    override fun onBindViewHolder(p0: TextHolder, p1: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun onBindViewHolder(holder: EmojiAdapter.TextHolder, position: Int) {
+        holder.bindEmoji("\uD83D\uDE04")
+
+
     }
 
     class TextHolder(v: View) : RecyclerView.ViewHolder(v), View.OnClickListener {
+        var view : View = v
+        var emoji : String = ""
+
+        fun bindEmoji(emoji:String) {
+            this.emoji = emoji
+            view.itemTextView.text = emoji
+        }
+
         override fun onClick(v: View?) {
             TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
         }
